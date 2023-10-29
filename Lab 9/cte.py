de0b2d28bce51e163c7fa22b2dd4c867481e2493
Siri Ananya Yallapragada <ananyayallapragada@gmail.com>
2023-10-29 21:00:23 +0530

    .

---
+++ Lab 9/cte.py
@@ -0,0 +1,3 @@
+undefined_var = 42 
+eval("invalid syntax")
+result = "5" + 2 
---
+++ Lab 9/rte.py
@@ -0,0 +1,7 @@
+dividend = 10
+divisor = 0
+result = dividend / divisor
+
+index = 5
+my_list = [1, 2, 3]
+element = my_list[index]
